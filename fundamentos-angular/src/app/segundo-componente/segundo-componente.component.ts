2c3a833b327744be434dc82b8f753ea344982800
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-01-20 16:22:52 -0300

    Primeiros passos no Angular: criando componentes

--- fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -1,11 +1,14 @@
+
 import { Component } from '@angular/core';
+import { UpperCasePipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-segundo-componente',
-  imports: [],
+  imports: [UpperCasePipe,DatePipe],
   templateUrl: './segundo-componente.component.html',
   styleUrl: './segundo-componente.component.css'
 })
 export class SegundoComponenteComponent {
-  
+  nome = 'João';
+  datanascimento = '1995-01-01';
 }
