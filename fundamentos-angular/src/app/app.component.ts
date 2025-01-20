ba51bdaaeb44dd1ee5264707df7f41b53ff74112
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-01-20 16:45:01 -0300

    two-way data binding de dados

--- fundamentos-angular/src/app/app.component.ts
+++ fundamentos-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { PrimeiroComponenteComponent } from "./primeiro-componente/primeiro-componente.component";
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PrimeiroComponenteComponent,SegundoComponenteComponent],
+  imports: [FormsModule, PrimeiroComponenteComponent,SegundoComponenteComponent,TwoWayBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
