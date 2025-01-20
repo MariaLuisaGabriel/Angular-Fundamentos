6a9e3595e022a855537f1d1607b8593435a201a9
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-01-20 15:56:59 -0300

    Primeiros passos no Angular: criando componentes

---
+++ fundamentos-angular/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro-componente',
+  imports: [],
+  //templateUrl: './primeiro-componente.component.html',
+  //styleUrl: './primeiro-componente.component.css'
+  template: '<h2>Primeiro Componente</h2>',
+  styles: [' h2 { color: red; }']
+})
+export class PrimeiroComponenteComponent {
+
+}
