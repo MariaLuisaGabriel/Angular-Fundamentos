35e85ff6d13f35c10d7256ff9c3a94c6bf0e5c23
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-01-21 14:10:58 -0300

    lidando com redirecionamento

---
+++ roteamento/src/app/pag-nao-encontrada/pag-nao-encontrada.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pag-nao-encontrada',
+  imports: [],
+  templateUrl: './pag-nao-encontrada.component.html',
+  styleUrl: './pag-nao-encontrada.component.css'
+})
+export class PagNaoEncontradaComponent {
+
+}
